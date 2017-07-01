e927e3a0b00d6d4123c717db934a0952860d0c47
abbach_n <nabil.abbach@epitech.eu>
2017-07-02 01:07:11 +0200

    Firt Push
    Pre bêta

---
+++ Client_UWP/HelloWorld(blank)/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloWorld_blank_
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // ici lancement de la procedure de connexion
+            // ajouter le using correspondant
+            // data.sqlClient;
+            /*
+             *   var test = new MessageDialog("id" + textBox.Text + " et mdps" + passwordBox.Password.ToString() + "Balek" );
+                 await test.ShowAsync();
+             */
+
+            String id = textBox.Text;
+            String mdps = passwordBox.Password.ToString();
+
+            // ici lancez la connexion a la base de donnée
+            // SqlConnection co = new SqlConnection();
+            //SqlDataAdapter data = new SqlDataAdapter("Select * From Databases where Login=" + id);//requete sql
+
+            // if pour comparer 
+
+            //Home home = new Home();
+            //await home.Show
+
+
+            Frame.Navigate(typeof(Page1));
+
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(inscription));
+        }
+    }
+}
---
+++ Client_UWP/HelloWorld(blank)/Menu_jeu_fail.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using Windows.UI.Popups;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HelloWorld_blank_
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class Menu_jeu_fail : Page
+    {
+        public Menu_jeu_fail()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //handle first game
+            //   C:\Users\Nabil\Desktop\Correction\Builds\Windows\Chessaken.exe
+
+            //DefaultLaunch();
+            
+        }
+
+        
+        async void DefaultLaunch()
+        {
+            // Path to the file in the app package to launch
+            //string exeFile = @"C:\Users\Nabil\Desktop\Correction\Builds\Windows\Chessaken.exe";
+
+            string exeFile = @"C:\Users\Nabil\Desktop\Correction\Builds\Windows\diag.png";
+
+            var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(exeFile);
+
+
+            
+            if (file != null)
+            {
+                // Launch the retrieved file
+                var success = await Windows.System.Launcher.LaunchFileAsync(file);
+
+
+
+                if (success)
+                {
+                    // File launched
+                    var dialog = new MessageDialog("Lancement");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    // File launch failed
+                    var dialog = new MessageDialog("Fail");
+                    await dialog.ShowAsync();
+                }
+            }
+            else
+            {
+                // Could not find file
+                var dialog = new MessageDialog("Marche pas");
+                await dialog.ShowAsync();
+            }
+            
+
+        }
+
+    }
+}
+
